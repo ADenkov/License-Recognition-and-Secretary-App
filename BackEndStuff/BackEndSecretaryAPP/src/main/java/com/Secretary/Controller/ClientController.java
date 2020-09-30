1e643e5fd4506baf5519812ac45635524a0b4c35
Konstantin Hadjiyankov <k.hadjiyankov@student.fontys.nl>
2020-10-01 00:32:52 +0200

    Get operations, fake DB, GET client by ID

---
+++ BackEndStuff/BackEndSecretaryAPP/src/main/java/com/Secretary/Controller/ClientController.java
@@ -0,0 +1,35 @@
+package com.Secretary.Controller;
+
+import com.Secretary.Entity.Client;
+import com.Secretary.Service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+
+@RestController
+@RequestMapping("/clients")
+
+public class ClientController {
+    @Autowired
+    private ClientService clientService;
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Collection<Client> getAllClients() {
+        return clientService.getAllClients();
+    }
+
+//    @RequestMapping(value = "/{id}", method = RequestMethod.GET ){
+//        public Client getClientById(@PathVariable("id") int id){
+//            return clientService.getClientById(id);
+//        }
+//    }
+    @GetMapping(path = "/{id}")
+        public Client getClientById(@PathVariable("id") int id){
+        return clientService.getClientById(id);
+    }
+
+
+}
