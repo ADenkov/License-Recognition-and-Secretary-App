188c7e3da8554698b5ea4ae37be8a2434782df0b
Jaklin <jakitoo99@gmail.com>
2020-09-30 22:05:59 +0200

    Front-end upload Pt2

---
+++ secretary-app-react/src/ButtonPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+function ButtonPage() {
+  return (
+    <center>
+      <>
+        <button variant="primary" size="lg" active>
+          Primary button
+        </button>{" "}
+        <button variant="secondary" size="lg" active>
+          Button
+        </button>
+      </>
+    </center>
+  );
+}
+export default ButtonPage;
